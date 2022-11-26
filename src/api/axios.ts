a5ac833230a778c1acb8a95c20c5571fe3f627dc
Kronos <1963024305@qq.com>
2022-11-26 14:26:38 +0800

    1

---
+++ src/api/axios.ts
@@ -0,0 +1,10 @@
+import axios from "axios";
+import {AxiosRequestConfig} from "axios";
+
+const Axios = (axiosConfig: AxiosRequestConfig<any>)=>{
+    const service = axios.create({
+        baseURL: "http://localhost:5000",    // 设置请求前缀
+        timeout: 100000,    //设置超时时长
+    })
+    return service(axiosConfig)
+}
