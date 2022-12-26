732129b1bea00d74df3d790f6da693365fe75977
Kronos <1963024305@qq.com>
2022-12-26 17:06:30 +0800

    finish graph item style

--- src/components/common/graph-item.tsx
+++ src/components/common/graph-item.tsx
@@ -17,6 +17,8 @@ const GraphItem: React.FC<GraphItemProps> = (props) => {
     const { width, height, isTarget, graph, setTarget, target} = props;
     const [initGraph, setInitGraph] = useState<Community|undefined>(graph);
     const nodeNumRef:LegacyRef<SVGSVGElement> = createRef();
+    const graphInfoRef:LegacyRef<SVGSVGElement> = createRef();
+
     useEffect(()=>{
         if(target!=null){
             setInitGraph(target);
@@ -31,31 +33,63 @@ const GraphItem: React.FC<GraphItemProps> = (props) => {
 
     useEffect(()=>{
         initNodeNum();
+        initGraphInfo();
     },[initGraph]);
 
     const initNodeNum = () => {
         const svg = d3.select(nodeNumRef?.current);
-        const data: number|undefined = initGraph?.node_num;
-        console.log(data);
-        
-        const min = 0, max = 3000;
-        const normalize = d3.scaleLinear().domain([min, max]).range([0, 1]); // linear
-        svg.append('rect')
-            .datum(data)
-            .attr('width', '100%')
-            .attr('height', '60%')
-            .attr('y','20%')
-            .attr('fill',(d: any, i: any)=>{
-                return d3.interpolateRgb('#2a71ae','#b82d35')(normalize(d));
-            });
-        svg.append('text')
-            .datum(data)
-            .style('fill','#fff')
-            .style('font-weight', 500)
-            .style('font-size','1rem')
-            .attr('x',"25%")
-            .attr('y',"62%")
-            .text((d: any)=>`${d?.toString()} nodes`);
+        if(initGraph != undefined){
+            const data: number|undefined = initGraph.node_num;
+            const min = 0, max = 3000;
+            const normalize = d3.scaleLinear().domain([min, max]).range([0, 1]); // linear
+            svg.append('rect')
+                .datum(data)
+                .attr('width', '100%')
+                .attr('height', '60%')
+                .attr('y','20%')
+                .attr('fill',(d: any, i: any)=>{
+                    return d3.interpolateRgb('#2a71ae','#b82d35')(normalize(d));
+                });
+            svg.append('text')
+                .datum(data)
+                .style('fill','#fff')
+                .style('font-weight', 500)
+                .style('font-size','1rem')
+                .attr('x',"25%")
+                .attr('y',"62%")
+                .text((d: any)=>`${d?.toString()} nodes`);
+        }
+    }
+
+    const initGraphInfo = () => {
+        const colorArray = ['#f49c84','#099EDA','#FEE301','#ABB7BD','#F4801F','#D6C223','#D75D73','#E0592B', '#58B7B3'];
+        const svg = d3.select(graphInfoRef?.current);
+        if(initGraph != undefined){
+            const data = initGraph.wrong_list;
+            const sum = d3.sum(data);
+            const band = ['porn','gambling','fraud','drug','gun','hacker','trading','pay','other'];
+            const cleanData = data.map((d:number, i:number)=>{if(d!=0){return {"name":band[i],"val":d/sum}}}).filter((d)=>{return d!= undefined});
+            const scaleBand = d3.scaleBand().domain(band).range([0,9]);
+            if(cleanData.length != 0){
+                svg.selectAll('rect')
+                .data(cleanData)
+                .join('rect')
+                .attr('width',(d:any)=>`${d.val*100}%`)
+                .attr('height','60%')
+                .attr('y',"20%")
+                .attr('x',(d: any, id: number)=>{
+                    if(id!=0){
+                        const dataTemp = cleanData.slice(0, id);
+                        const dataTempSum = dataTemp.reduce((pre: any, cur:any)=>{return pre+cur.val}, 0);
+                        return `${dataTempSum*100}%`;
+                    }else{
+                        return 0;
+                    }
+                }).attr('fill',(d: any)=>{
+                    return colorArray[scaleBand(d.name) as number];
+                });
+            }
+        }
     }
 
     return (
@@ -67,8 +101,8 @@ const GraphItem: React.FC<GraphItemProps> = (props) => {
                 <Col span={8} style={{height:"100%"}}>
                     <svg ref={nodeNumRef} id='node-num' width={"100%"} height={"100%"}></svg>
                 </Col>
-                <Col span={12} style={{height:"100%"}}>
-                    <svg id='graph-info' width={"100%"} height={"100%"}></svg>
+                <Col span={10} style={{height:"100%"}}>
+                    <svg ref={graphInfoRef} width={"100%"} height={"100%"}></svg>
                 </Col>
             </Row>
        </div>);
