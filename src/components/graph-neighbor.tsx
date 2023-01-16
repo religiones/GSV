39d69822eb050942053ad78395dcb0ae334ee066
Kronos <1963024305@qq.com>
2023-01-16 18:07:11 +0800

    修改邻居图为堆叠环图

--- src/components/graph-neighbor.tsx
+++ src/components/graph-neighbor.tsx
@@ -1,6 +1,6 @@
 import G6, { Graph, GraphData, IG6GraphEvent, NodeConfig } from '@antv/g6';
-import { scaleLinear, min, max, dispatch } from 'd3';
-import React, { useEffect, useState } from 'react';
+import * as d3 from 'd3';
+import React, { createRef, LegacyRef, useEffect, useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { setSelectCommunities } from '../store/features/community-list-slice';
 import { Community } from './@types/communi-list';
@@ -9,27 +9,15 @@ import "./style/graph-view.less";
 let graph: Graph|null = null;
 const GraphNeighbor: React.FC<{}> = () => {
     const { currentCommunity, communities, selectCommunities} = useSelector((store: any) => store.communityList);
-    const [neighborCommunity, setNeighborCommunity] = useState<Community[]>([]);
+    const graphRef:LegacyRef<SVGSVGElement> = createRef();
     const dispatch = useDispatch();
     let selectNeighbor:Community[] = [];
 
-    // useEffect(()=>{
-    //     initGraph(currentCommunity.id, currentCommunity.neighbour);
-    // },[currentCommunity.id]);
-    
     useEffect(()=>{
-        if(graph != null && neighborCommunity.length != 0){
-            graph.on("node:dblclick",(e: IG6GraphEvent)=>{
-                const graph = e.item?.getModel();
-                neighborCommunity.forEach((community:Community)=>{
-                    if(community.id === Number(graph?.id)){
-                        selectNeighbor.push(community);
-                    }
-                });
-                dispatch(setSelectCommunities({selectCommunities:[...selectCommunities, ...selectNeighbor]}));
-            });
-        }
-    },[neighborCommunity])
+        // initGraph(currentCommunity.id, currentCommunity.neighbour);
+        initNeighborGraph(currentCommunity.id, currentCommunity.neighbour);
+    },[currentCommunity.id]);
+    
 
     // const initGraph = (id: number, neighbors: number[]) => {
     //     let data: GraphData = {
@@ -132,14 +120,88 @@ const GraphNeighbor: React.FC<{}> = () => {
     //     }
     // }
 
-    const initNeighborGraph = (id:number, neighbor: number[])=>{
-        
+
+
+    const initNeighborGraph = (id:number, neighbors: number[])=>{
+        let neighborCommunity:Community[] = [];
+        let currentCommunity:Community|undefined = undefined;
+        for(const community of communities){
+            if(neighbors.includes(community.id)){
+                neighborCommunity.push(community);
+            }
+            if(id === community.id){
+                currentCommunity =community;
+            }
+        }
+        const svg = d3.select(graphRef?.current);
+        const container = document.getElementById("neighbor-container");
+        const width = container?.clientWidth as number;
+        const height = container?.clientHeight as number;
+        const padding = 10;
+        const innerRadius = 100;
+        const outerRadius = Math.min(width,height)/2-padding;
+        svg.style("font", "10px sans-serif")
+            .attr("viewBox",`${-width/2} ${-height*0.6} ${width} ${height}`);
+        svg.selectChildren().remove();
+        if(neighborCommunity.length != 0){
+            const keys = ["porn","gambling","fraud","drug","gun","hacker","trading","pay","other"];
+            const colorArray = ['#f49c84','#099EDA','#FEE301','#ABB7BD','#F4801F','#D6C223','#D75D73','#E0592B', '#58B7B3'];
+            const colorScale = d3.scaleOrdinal().domain(keys).range(colorArray);
+            const stack = d3.stack().keys(keys);
+            const neighborId:string[] = neighborCommunity.map((community:Community)=>String(community.id));
+            const xScale = d3.scaleBand().domain(neighborId).range([0,2*Math.PI]).align(0);
+            const yScale = d3.scaleSqrt().domain([0, d3.max(neighborCommunity,(d:any)=>d.wrong_num)]).range([innerRadius, outerRadius]);
+            const arc = d3.arc()
+                .innerRadius((d:any)=>yScale(d[0]))
+                .outerRadius((d:any)=>yScale(d[1]))
+                .startAngle((d:any)=>xScale(String(d.data.id)) as number)
+                .endAngle((d:any)=>xScale(String(d.data.id)) as number + xScale.bandwidth())
+                .padAngle(0.01)
+                .padRadius(innerRadius);
+
+            svg.append("g")
+                .selectAll("g")
+                .data(stack(neighborCommunity as Iterable<{ [key: string]: number; }>))
+                .join("g")
+                .attr("fill",(d: any):string=>colorScale(d.key) as string)
+                .selectAll("path")
+                .data(d=>d)
+                .join("path")
+                .attr("d", (d:any)=>arc(d));
+            const xAxis = (g:any) => g
+                .attr("class","xAxis")
+                .attr("text-anchor", "middle")
+                .call((g:any) => g.selectAll("g")
+                .data(neighborCommunity)
+                .enter().append("g")
+                .attr("transform", (d:any) => `
+                rotate(${((xScale(String(d.id)) as number + xScale.bandwidth() / 2) * 180 / Math.PI - 90)})
+                translate(${innerRadius},0)
+                `)
+                .call((g:any) => g.append("line")
+                    .attr("x2", -5)
+                    .attr("stroke", "#000"))
+                .call((g:any) => g.append("text")
+                .attr("transform", "rotate(180)translate(26,3)")
+                .text((d:any) => String(d.id))))
+                .style("cursor","pointer");
+            
+            svg.append("g").call(xAxis);
+            d3.selectAll("text")
+                .on("click",(e)=>{
+                    const community = e.target.__data__;
+                    if(!selectNeighbor.includes(community)){
+                        selectNeighbor.push(e.target.__data__);
+                        dispatch(setSelectCommunities({selectCommunities:[...selectCommunities, ...selectNeighbor]}));
+                    }
+                });
+        }
     }
 
     return (
-        <div className='graph-wrap'>
+        <div className='graph-wrap' style={{overflow:'hidden'}}>
             <span id='graph-title'>{`community: ${currentCommunity===null?'null':currentCommunity.id}`}</span>
-            <div id='neighbor-container' style={{width:'100%', height:'100%', overflow:'hidden'}}></div>
+            <svg id='neighbor-container' ref={graphRef} style={{width:'100%', height:'100%'}}></svg>
         </div>
        );
 };
